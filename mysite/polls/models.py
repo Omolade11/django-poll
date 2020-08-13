76edc58d8301a6f1fedff81bf5a96d2f08fae22f
Omolade Ekpeni <omoladeekpeni@gmail.com>
2020-08-13 00:38:44 -0700

    django-poll

---
+++ mysite/polls/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+        # ...
+    def __str__(self):
+        return self.choice_text
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
